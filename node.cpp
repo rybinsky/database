20d7dd2ab4d6196da9c6b51dd86759b7c9a6c63e
rybinsky <70236307+rybinsky@users.noreply.github.com>
2022-04-10 11:57:41 +0300

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -6,14 +6,14 @@ bool EmptyNode::Evaluate(const Date& d, const string& ev) { return true; }
 
 DateComparisonNode::DateComparisonNode(const Comparison& op, const Date& d) :op_(op), d_(d) {}
 
-bool DateComparisonNode::Evaluate(const Date& d, const string& ev) {
+bool DateComparisonNode::Evaluate(const Date& d, const string& ev) { // вычислить звено с датами
 	return Comp(op_, d, d_);
 }
 
 
 EventComparisonNode::EventComparisonNode(const Comparison& op, const string& ev) :op_(op), ev_(ev) {}
 
-bool EventComparisonNode::Evaluate(const Date& d, const string& ev) {
+bool EventComparisonNode::Evaluate(const Date& d, const string& ev) { // вычислить звено с событиями
 	if (ev.empty()) {
 		return 0;
 	}
@@ -25,17 +25,17 @@ LogicalOperationNode::LogicalOperationNode(const LogicalOperation& op,
 	const shared_ptr<Node>& left, const shared_ptr<Node>& right)
 	: op_(op), left_(left), right_(right) {}
 
-		bool LogicalOperationNode::Evaluate(const Date& d, const string& ev) {
-			if (op_ == LogicalOperation::And) {
-				return left_->Evaluate(d, ev) && right_->Evaluate(d, ev);
-			}
-			if (op_ == LogicalOperation::Or) {
-				return left_->Evaluate(d, ev) || right_->Evaluate(d, ev);
-			}
-			return 0;
-		}
+bool LogicalOperationNode::Evaluate(const Date& d, const string& ev) { // вычислить логическую операцию
+	if (op_ == LogicalOperation::And) {
+		return left_->Evaluate(d, ev) && right_->Evaluate(d, ev);
+	}
+	if (op_ == LogicalOperation::Or) {
+		return left_->Evaluate(d, ev) || right_->Evaluate(d, ev);
+	}
+	return 0;
+}
 
-		void LogicalOperationNode::SetLeft(shared_ptr<Node> node) { left_ = node; }
-		void LogicalOperationNode::SetRight(shared_ptr<Node> node) { right_ = node; }
+void LogicalOperationNode::SetLeft(shared_ptr<Node> node) { left_ = node; }
+void LogicalOperationNode::SetRight(shared_ptr<Node> node) { right_ = node; }
 
 
